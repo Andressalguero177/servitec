11c5fc658be09584e6a50a7e9533777ec0674b26
Andres salguero <cc925721@gmail.com>
2025-06-14 22:13:12 -0500

    primer vistas y prototipo del navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+  const [menuAbierto, setMenuAbierto] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuAbierto(!menuAbierto)
+  }
+
+  return (
+    <nav className="navbar">
+      <Link to="/" className="logo">MiTienda</Link>
+
+      <div className={`menu-toggle ${menuAbierto ? 'open' : ''}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`nav-links ${menuAbierto ? 'active' : ''}`}>
+        <li><NavLink to="/" end onClick={() => setMenuAbierto(false)}>Inicio</NavLink></li>
+        <li><NavLink to="/about" onClick={() => setMenuAbierto(false)}>About</NavLink></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+
+
+function Home() {
+  return (
+      <div>
+
+        <div id="hero">
+          <h1>Servitec</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia optio sed, tempora neque soluta temporibus animi magni ab placeat ex dolorum totam quam, pariatur architecto quis et distinctio voluptas unde!</p>
+        </div>
+        <div className="container">
+          <h3>funciones</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime error nostrum odit rem saepe beatae omnis corporis itaque unde cumque, quis quod laborum assumenda voluptatem? Culpa rerum reiciendis optio dolorem.</p>
+          <h1>img</h1>
+        </div>
+        <div>
+          <h1>card</h1>
+          <h1>card</h1>
+          <h1>card</h1>
+          <h1>card</h1>
+        </div>
+      </div>
+  )
+}
+
+export default Home
