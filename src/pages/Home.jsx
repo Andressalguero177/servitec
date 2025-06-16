181c7a6f672f03e7f99348cef9b98cd590e85e46
Andres salguero <cc925721@gmail.com>
2025-06-16 12:49:36 -0500

    avances en el home para la pagina servitec

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,49 @@
-
-
+import '../styles/Home.css'
 function Home() {
   return (
-      <div>
-
+      <div className='container-hero'>
         <div id="hero">
           <h1>Servitec</h1>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia optio sed, tempora neque soluta temporibus animi magni ab placeat ex dolorum totam quam, pariatur architecto quis et distinctio voluptas unde!</p>
+          <button>CONTACTANOS YA!</button>
         </div>
-        <div className="container">
-          <h3>funciones</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime error nostrum odit rem saepe beatae omnis corporis itaque unde cumque, quis quod laborum assumenda voluptatem? Culpa rerum reiciendis optio dolorem.</p>
-          <h1>img</h1>
+        <div id="somos-proya">
+          <div class="container">
+            <div class="img-container"></div>
+            <div class="texto">
+
+                <h2>Somos <span class="color-acento">servitec!</span></h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit veritatis a autem sed dignissimos commodi, voluptas incidunt voluptatibus cum, quia neque nulla consequatur laborum accusamus, perspiciatis odio nemo minus vel!</p>
+            </div>
         </div>
-        <div>
-          <h1>card</h1>
-          <h1>card</h1>
-          <h1>card</h1>
-          <h1>card</h1>
         </div>
+        <div id="nuestros-programas">
+          <div class="container">
+              <h2>Nuestros servicios</h2>
+              <div class="programas">
+                  <div class="carta">
+                      <h3>Funcion 1</h3>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vero corporis incidunt saepe qui commodi quasi neque veniam quam, aspernatur est beatae maxime animi sed reiciendis mollitia ducimus veritatis repellendus?</p>
+                      <button>+ Info</button>
+                  </div>
+                  <div class="carta">
+                      <h3>Funcion 2</h3>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vero corporis incidunt saepe qui commodi quasi neque veniam quam, aspernatur est beatae maxime animi sed reiciendis mollitia ducimus veritatis repellendus?</p>
+                      <button>+ Info</button>
+                  </div>
+                  <div class="carta">
+                      <h3>Funcion 3</h3>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vero corporis incidunt saepe qui commodi quasi neque veniam quam, aspernatur est beatae maxime animi sed reiciendis mollitia ducimus veritatis repellendus?</p>
+                      <button>+ Info</button>
+                  </div>  
+              </div>
+          </div>
+        </div>
+        <footer>
+          <div class="container">
+            <p>&copy; Servitec 2021</p>
+          </div>
+        </footer>
       </div>
   )
 }
